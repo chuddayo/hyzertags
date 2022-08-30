701ce2534815f4c684654f5b606cc1ef0e831bbf
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-30 16:40:42 -0400

    add player functionality finished

--- src/components/LeagueSelection.js
+++ src/components/LeagueSelection.js
@@ -12,11 +12,16 @@ function LeagueSelection({
 }) {
   // keeps track of value of selected league in dropdown
   const [selectedLeague, setSelectedLeague] = useState("");
+  // toggles add player text-input field
   const [showAddPlayerInput, setShowAddPlayerInput] = useState(false);
+  // keeps track of value of new player name in text-input field
+  const [newPlayerName, setNewPlayerName] = useState("");
+  // keeps track of next tag to be assigned in league
   const [nextTag, setNextTag] = useState(0);
 
+  // initial component GET request for a league's next to be assigned tag number
   useEffect(() => {
-    if(selectedLeague !== "") {
+    if (selectedLeague !== "") {
       async function fetchNextTag() {
         const request = await axios.get(
           `${requests.fetchLeagues}/${selectedLeague}/nexttag`
@@ -49,28 +54,57 @@ function LeagueSelection({
       });
   };
 
-  const handlePlayerAddition = () => {
-    setShowAddPlayerInput(!showAddPlayerInput);
-    console.log(leagueStandings);
-    console.log(nextTag);
-    // setNextTag(nextTag + 1);
+  const handleAddPlayerNameChange = (e) => {
+    setNewPlayerName(e.target.value);
   };
 
+  async function addPlayerToDB() {
+    let newPlayer = await axios.post(`${requests.fetchPlayers}`, {
+      firstName: newPlayerName.substring(0, newPlayerName.indexOf(" ")),
+      lastName: newPlayerName.substring(newPlayerName.indexOf(" ") + 1)
+    });
+    return newPlayer;
+  }
+
+  const handlePlayerAddition = () => {
+    setShowAddPlayerInput(!showAddPlayerInput);
+    if (newPlayerName !== "") {
+      addPlayerToDB().then((newPlayer) => {
+        handleLeagueStandingsChange(
+          leagueStandings.concat({
+            leagueID: parseInt(selectedLeague),
+            playerTag: nextTag,
+            player: newPlayer.data,
+            selected: false,
+            strokesToPar: null,
+          })
+        );
+        setNextTag(nextTag + 1);
+        setNewPlayerName(""); 
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  };
 
   return (
     <div>
       {/* Dropdown Select */}
-      {!showAddPlayerInput && <><h4>select league to see standings</h4>
-      <div className="custom-select">
-        <select value={selectedLeague} onChange={leagueChange}>
-          <option key="0">-- Select League --</option>
-          {leagues.map((league) => (
-            <option key={league.id} value={league.id}>
-              {league.name}
-            </option>
-          ))}
-        </select>
-      </div></>}
+      {!showAddPlayerInput && (
+        <>
+          <h4>select league to see standings</h4>
+          <div className="custom-select">
+            <select value={selectedLeague} onChange={leagueChange}>
+              <option key="0">-- Select League --</option>
+              {leagues.map((league) => (
+                <option key={league.id} value={league.id}>
+                  {league.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </>
+      )}
 
       {/* Standings Table */}
       {selectedLeague && (
@@ -104,19 +138,23 @@ function LeagueSelection({
         </table>
       )}
 
-      {selectedLeague && <>
-        {showAddPlayerInput && <input
-          placeholder="First Last"
-          className="text-input"
-          // onChange={(e) => {
-          //   handleEventNameChange(e);
-          // }}
-        />}
-        <div className="flex-buttons">
-          <button onClick={handlePlayerAddition}>Add Player</button>
-          <button onClick={eventTriggerClick}>Create Event</button>
-        </div>
-      </>}
+      {selectedLeague && (
+        <>
+          {showAddPlayerInput && (
+            <input
+              placeholder="First Last"
+              className="text-input"
+              onChange={(e) => {
+                handleAddPlayerNameChange(e);
+              }}
+            />
+          )}
+          <div className="flex-buttons">
+            <button onClick={handlePlayerAddition}>Add Player</button>
+            <button onClick={eventTriggerClick}>Create Event</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
